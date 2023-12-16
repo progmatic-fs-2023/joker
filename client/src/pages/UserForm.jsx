0886411d640deb7c53c833bf52680256a3d564de
Dora Korsos <korsosdora@gmail.com>
2023-12-16 01:02:45 +0100

    Shipping form with eslint fixes

--- client/src/pages/UserForm.jsx
+++ client/src/pages/UserForm.jsx
@@ -24,85 +24,117 @@ function UserForm() {
     <div className="form-container">
       <form className="shipping-form">
         <h3>Kapcsolattartási adatok</h3>
+        <div className="input-container">
+          <label htmlFor="first-name">
+            Keresztnév
+            <input
+              type="text"
+              id="first-name"
+              className="input-field first-name"
+              onChange={handleChange}
+              name="firstName"
+              placeholder="Keresztnév"
+              required
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="last-name">
+            Vezetéknév
+            <input
+              type="text"
+              id="last-name"
+              className="input-field last-name"
+              onChange={handleChange}
+              name="lastName"
+              placeholder="Vezetéknév"
+              required
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              id="email"
+              onChange={handleChange}
+              className="input-field email"
+              name="email"
+              placeholder="pl.:felhasznalo@joker.com"
+              required
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="phone-number">
+            Telefonszám
+            <input
+              type="tel"
+              id="phone-number"
+              className="input-field phone-number"
+              onChange={handleChange}
+              name="phoneNumber"
+              placeholder="06201234567"
+              required
+            />
+          </label>
+        </div>
 
-        <label htmlFor="first-name">Keresztnév</label>
-        <input
-          type="text"
-          id="first-name"
-          className="input-field first-name"
-          onChange={handleChange}
-          name="firstName"
-          placeholder="Keresztnév"
-          required
-        />
-        <label htmlFor="last-name">Vezetéknév</label>
-        <input
-          type="text"
-          id="last-name"
-          className="input-field last-name"
-          onChange={handleChange}
-          name="lastName"
-          placeholder="Vezetéknév"
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          onChange={handleChange}
-          className="input-field email"
-          name="email"
-          placeholder="pl.:felhasznalo@joker.com"
-          required
-        />
-        <label htmlFor="phone-number">Telefonszám</label>
-        <input
-          type="tel"
-          id="phone-number"
-          className="input-field phone-number"
-          onChange={handleChange}
-          name="phoneNumber"
-          placeholder="06201234567"
-          required
-        />
         <h3>Szállítási cím</h3>
-        <label htmlFor="adress">Cím</label>
-        <input
-          type="text"
-          id="adress"
-          className="input-field adress"
-          onChange={handleChange}
-          name="adress"
-          placeholder="utca, házszám, emelet, ajtó"
-          required
-        />
-        <label htmlFor="zip-code">Irányítószám</label>
-        <input
-          type="text"
-          id="zip-code"
-          className="input-field zip"
-          onChange={handleChange}
-          name="zipCode"
-          required
-        />
-        <label htmlFor="city">Város</label>
-        <input
-          type="text"
-          id="city"
-          className="input-field city"
-          onChange={handleChange}
-          name="city"
-          required
-        />
-        <label htmlFor="country">Ország</label>
-        <input
-          type="text"
-          id="country"
-          className="input-field country"
-          onChange={handleChange}
-          name="country"
-          required
-        />
+        <div className="input-container">
+          <label htmlFor="adress">
+            Cím
+            <input
+              type="text"
+              id="adress"
+              className="input-field adress"
+              onChange={handleChange}
+              name="adress"
+              placeholder="utca, házszám, emelet, ajtó"
+              required
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="zip-code">
+            Irányítószám
+            <input
+              type="text"
+              id="zip-code"
+              className="input-field zip"
+              onChange={handleChange}
+              name="zipCode"
+              required
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="city">
+            Város
+            <input
+              type="text"
+              id="city"
+              className="input-field city"
+              onChange={handleChange}
+              name="city"
+              required
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="country">
+            Ország
+            <input
+              type="text"
+              id="country"
+              className="input-field country"
+              onChange={handleChange}
+              name="country"
+              required
+            />
+          </label>
+        </div>
         <div className="customer-checkbox">
           <input type="checkbox" name="aszf" id="terms_id" required />
           <label htmlFor="terms_id">
@@ -114,7 +146,9 @@ function UserForm() {
             .
           </label>
         </div>
-        <button className="pay-button fullwidth">Fizetés és megrendelés</button>
+        <button type="button" className="pay-button fullwidth">
+          Fizetés és megrendelés
+        </button>
       </form>
       {/* <div>Ide jöhetne a jelenleg kosárban lévő termékek megjelenítése.</div> */}
     </div>
