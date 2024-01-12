5b7abd808ece686d9bff50f457bd759c20ac6507
Akos1989 <akos.badar@gmail.com>
2024-01-12 18:27:50 +0100

    user can edit his profile

--- client/src/pages/UsersPage.jsx
+++ client/src/pages/UsersPage.jsx
@@ -1,68 +1,72 @@
-import Login from "../components/Login";
+import React, { useState } from 'react';
 
 function UsersPage() {
+    const [currentUser, setUser] = useState({
+        id: "alskdasdalskdjl",
+        firstName: "Firstname",
+        lastName: "Lastname",
+        address: "Budapest",
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div>
-            <form>
-  <div class="form-row">
-    <div class="col">
-      <input type="text" class="form-control" placeholder="First name"></input>
-    </div>
-    <div class="col">
-      <input type="text" class="form-control" placeholder="Last name"></input>
-    </div>
-  </div>
-</form>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"></input>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password"></input>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"></input>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity"></input>
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip"></input>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"></input>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Edit profile</button>
-</form>
+            <h1>User Profile</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    First Name:
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={currentUser.firstName}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Last Name:
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={currentUser.lastName}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Address:
+                    <input
+                        type="text"
+                        name="address"
+                        value={currentUser.address}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Save Changes</button>
+            </form>
+
+
+        <div>
+
+        First Name:{currentUser.firstName} <br />
+        Last Name:{currentUser.lastName}<br />
+        Address:{currentUser.address}
+        </div>
+
         </div>
     );
 }
 
-
 export default UsersPage;
-
