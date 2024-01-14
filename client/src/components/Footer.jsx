331d83b3f443600d9cad6b77bbd183d50c6f8267
Janos_Vona <vonajanos3@gmail.com>
2024-01-14 20:28:59 +0100

    feat: implemented footer,aszf

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,22 @@
-
 function Footer() {
   return (
     <div className="d-flex justify-content-center text-center">
       Herbalism.hu&copy; gyógynövény webshop. Minden jog fenntartva!
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">Rólunk</a>
+          </li>
+          <li>
+            <a href="#contact">Kapcsolat</a>
+          </li>
+          <li>
+            <a href="#contact">ÁSZF</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import ImageSlider from '../components/ImageSlider';
-
+import InformationSlide from '../components/InformationSlide';
 function Home() {
-    return (
-        <div className="d-flex row text-center m-1">
-            <h1>Főoldal</h1>
-            <ImageSlider />
-        </div>
-    );
+  return (
+    <div className="d-flex row text-center m-1">
+      <h1>Főoldal</h1>
+      <ImageSlider />
+    </div>
+  );
 }
 
 export default Home;
