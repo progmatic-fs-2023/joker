9d6f20d79010626e3d7328ba5f7ce41313e8ce2f
Janos Panic <janos.panic@gmail.com>
2024-01-25 20:19:18 +0100

    [JOK-57]Create_Search_Algorythm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,6 +32,7 @@ import UserOrderList from './components/user/UserOrderList';
 import HerbEditor from './components/user/HerbEditor';
 import OrderEditor from './components/order/OrderEditor';
 import Missing from './components/blog/Missing';
+import ASZF from './pages/Aszf';
 import SearchPage from './pages/SearchPage';
 
 function App() {
@@ -54,6 +55,7 @@ function App() {
               <Route path="sitemap" element={<Sitemap />} />
               <Route path="payment" element={<Payment />} />
               <Route path="logout" element={<Logout />} />
+              <Route path="aszf" element={<ASZF />} />
               {/* public auth routes */}
               <Route path="login" element={<Login />} />
               <Route path="register" element={<Register />} />
