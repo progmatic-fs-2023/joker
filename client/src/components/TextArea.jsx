8914e3634cf94459f8dc5e1179a1d409a3821d4e
Dora Korsos <korsosdora@gmail.com>
2024-01-25 22:28:23 +0100

    Main page card text added

--- client/src/components/TextArea.jsx
+++ client/src/components/TextArea.jsx
@@ -6,27 +6,25 @@ function TextArea() {
     <div className="shadow-lg p-2 mb-5 rounded">
       <Card className="text-center bg-dark text-white">
         <Card.Header className="fw-bold" as="h4">
-          Bazi jó cím a kártyának
+          A gyógynövényhasználat alapjai
         </Card.Header>
         <Card.Body>
           <Card.Text as="text">
             <p className="fs-5">
-              Herbalism egy külföldi eredetű szó jelentése: A növények ismeretének és gyógyító
-              tulajdonságaik felhasználásának tudománya.A gyógynövények használata több ezer évre
-              visszavezethető. herbalizmus egyedülálló módon ötvözi a kulturális hagyományokat, az
+              Herbalism egy külföldi eredetű szó, jelentése: A növények ismeretének és gyógyító
+              tulajdonságaik felhasználásának tudománya. A gyógynövények használata több ezer évre
+              visszavezethető. A herbalizmus egyedülálló módon ötvözi a kulturális hagyományokat, az
               ételeket és a jó közérzetet mindenki számára.
             </p>
             <p className="fs-5">
-              Herbalism egy külföldi eredetű szó jelentése: A növények ismeretének és gyógyító
-              tulajdonságaik felhasználásának tudománya.A gyógynövények használata több ezer évre
-              visszavezethető. herbalizmus egyedülálló módon ötvözi a kulturális hagyományokat, az
-              ételeket és a jó közérzetet mindenki számára.
+              A farmakológiai kutatások világméretű elterjedésével néhány gyógynövényt modern
+              gyógymóddá alakítottak át, mint például az artemisinin nevű maláriaellenes
+              gyógyszercsoportot, amelyet az Artemisia annua (Egynyári üröm) nevű, a kínai
+              orvoslásban a láz kezelésére használt gyógynövényből izoláltak.
             </p>
             <p className="fs-5">
-              Herbalism egy külföldi eredetű szó jelentése: A növények ismeretének és gyógyító
-              tulajdonságaik felhasználásának tudománya.A gyógynövények használata több ezer évre
-              visszavezethető. herbalizmus egyedülálló módon ötvözi a kulturális hagyományokat, az
-              ételeket és a jó közérzetet mindenki számára.{' '}
+              A gyógynövények számos formában alkalmazhatók, amelyek közül a leggyakoribb a
+              gyógyteaként vagy (esetleg hígított) növényi kivonatként fogyasztott folyadék.{' '}
             </p>
           </Card.Text>
         </Card.Body>
